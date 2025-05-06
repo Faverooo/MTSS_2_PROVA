112a3b491959396435ff114ae823ebe3c7d9bc46
Angelica Gastaldello <angelicagastaldello@gmail.com>
2025-05-06 11:09:29 +0200

    Creazione file vuoto RP

---
+++ src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -0,0 +1,16 @@
+////////////////////////////////////////////////////////////////////
+// Marco Favero 2101071
+// Angelica Gastaldello 2111003
+////////////////////////////////////////////////////////////////////
+package it.unipd.mtss;
+
+public class RomanPrinter {
+    public static String print(int num) {
+        return printAsciiArt(IntegerToRoman.convert(num));
+    }
+
+    private static String printAsciiArt(String romanNumber) {
+        // TODO
+        return null;
+    }
+}
